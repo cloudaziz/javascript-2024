45b0fd4fef85f7c1e7e083ae1a6416455c46d5b6
Mohammed Abdul Aziz <cloudaziz@gmail.com>
2024-02-29 14:35:27 +0600

    add action of prevBtn

--- infiniteslider/script.js
+++ infiniteslider/script.js
@@ -12,14 +12,35 @@ slider.append(firstClone);
 slider.prepend(lastClone);
 
 let currentIndex = 1;
+
+prevBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  // for update sliderItems
+  sliderItems = slider.querySelectorAll('li');
+
+  // stop when last element is current item
+  if (currentIndex === 0) return;
+
+  // increment currentIndex
+  currentIndex = currentIndex - 1;
+
+  // move item with css custom property
+  slider.style.setProperty('--item-index', currentIndex);
+  slider.style.transition = '.7s';
+});
+
 nextBtn.addEventListener('click', (event) => {
   event.preventDefault();
   // for update sliderItems
   sliderItems = slider.querySelectorAll('li');
+
   // stop when last element is current item
   if (currentIndex === sliderItems.length - 1) return;
 
+  // increment currentIndex
   currentIndex = currentIndex + 1;
+
+  // move item with css custom property
   slider.style.setProperty('--item-index', currentIndex);
   slider.style.transition = '.7s';
 });
